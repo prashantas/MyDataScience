ed4dd5a571ca8327de71f217899fe3653e3defa3
Prashanta Sharma <prashanta.tezu@gmail.com>
2017-08-30 18:09:13 +0530

    Update DifferentProblems.py

--- DifferentProblems.py
+++ DifferentProblems.py
@@ -19,3 +19,34 @@ confusion_matrix(y_test, predict_mine)
 
 #array([[6927,  309],
 #       [ 621,  121]])
+
+##########################################################################################################################
+###########################################################################################################################
+  ### DRAWING roc CURVE **********************
+  #https://datamize.wordpress.com/2015/01/24/how-to-plot-a-roc-curve-in-scikit-learn/
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+import random
+
+#2) Generate actual and predicted values. First let use a good prediction probabilities array:
+
+actual = [1,1,1,0,0,0]
+predictions = [0.9,0.9,0.9,0.1,0.1,0.1]
+
+false_positive_rate, true_positive_rate, thresholds = roc_curve(actual, predictions)
+roc_auc = auc(false_positive_rate, true_positive_rate)
+##4) Finally we plot the fpr vs tpr as well as our auc for our very good classifier.
+
+plt.title('Receiver Operating Characteristic')
+plt.plot(false_positive_rate, true_positive_rate, 'b',
+label='AUC = %0.2f'% roc_auc)
+plt.legend(loc='lower right')
+plt.plot([0,1],[0,1],'r--')
+plt.xlim([-0.1,1.2])
+plt.ylim([-0.1,1.2])
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.show()
+
+###############################################################################################################
+################################################################################################################
