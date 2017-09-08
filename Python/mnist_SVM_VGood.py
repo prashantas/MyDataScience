7d9b11775395602beaa067e479dd6a727c0b5d3c
Prashanta Sharma <prashanta.tezu@gmail.com>
2017-09-09 05:13:48 +0530

    Create mnist_SVM_VGood.py

---
+++ Python/mnist_SVM_VGood.py
@@ -0,0 +1,28 @@
+###  Source ::  https://gist.github.com/amueller/2594372
+###  http://peekaboo-vision.blogspot.in/2010/09/mnist-for-ever.html
+
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import StratifiedKFold
+
+def main():
+    mnist = fetch_mldata("MNIST original")
+    X_all, y_all = mnist.data/255., mnist.target
+    print("scaling")
+    X = X_all[:60000, :]
+    y = y_all[:60000]
+
+    X_test = X_all[60000:, :]
+    y_test = y_all[60000:]
+
+    svm = SVC(cache_size=1000, kernel='rbf')
+
+    parameters = {'C':10. ** np.arange(5,10), 'gamma':2. ** np.arange(-5, -1)}
+    print("grid search")
+    grid = GridSearchCV(svm, parameters, cv=StratifiedKFold(y, 5), verbose=3, n_jobs=-1)
+    grid.fit(X, y)
+    print("predicting")
+    print "score: ", grid.score(X_test, y_test)
+    print grid.best_estimator_
+
+if __name__ == "__main__":
+    main()
