14d7271eeaf18ef0d03ae7c3a259dd049b85fd38
Prashanta Sharma <prashanta.tezu@gmail.com>
2017-12-07 15:40:24 +0530

    Update runTensorBoard.py

--- DeepNetwork/runTensorBoard.py
+++ DeepNetwork/runTensorBoard.py
@@ -8,9 +8,11 @@ sess = tf.Session()
 
 File_Writer = tf.summary.FileWriter('C:\\Users\\prassha\\Desktop\\MachineLearning\\EquiSkill\\TF_Sonar\\graph',sess.graph)
 
-## (C:\Users\prassha\AppData\Local\Continuum\Anaconda3) C:\Users\prassha\Desktop\MachineLearning\EquiSkill\TF_Sonar\graph>tensorboard --logdir="C:\Users\prassha\Desktop\MachineLearning\EquiSkill\TF_Sonar\graph"
+## Execute this program and Run the following command from Annaconda prompt (C:\Users\prassha\AppData\Local\Continuum\Anaconda3)
+##C:\Users\prassha\Desktop\MachineLearning\EquiSkill\TF_Sonar\graph>tensorboard --logdir="C:\Users\prassha\Desktop\MachineLearning\EquiSkill\TF_Sonar\graph"
 ## Starting TensorBoard b'54' at http://prassha-WX-1:6006
 ## (Press CTRL+C to quit)
+## Now open localhost:6006 and inside the graph tab, we can see the graph
 
 print(sess.run(c))
 
